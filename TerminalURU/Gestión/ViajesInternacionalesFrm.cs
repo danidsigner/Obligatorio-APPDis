73e2f565687c22ad78262a8f5892fa9207a6d6c1
Danilo Oliba <danilo.oliba@hotmail.com>
2018-11-05 18:27:42 -0300

    despues de la rama 2

--- TerminalURU/Gestión/ViajesInternacionalesFrm.cs
+++ TerminalURU/Gestión/ViajesInternacionalesFrm.cs
@@ -24,16 +24,6 @@ namespace Gestión
             InitializeComponent();
             empLogueado = emp;
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-        //comentario para ver cambios de github
-	//comentario git biran
-=======
-        //github
->>>>>>> parent of 5eb8830... segundo commit
-=======
-
->>>>>>> 67cc4db5cc238c3e5b7fa68a16148897710602ff
         private void ViajesInternacionales_Load(object sender, EventArgs e)
         {
             try
